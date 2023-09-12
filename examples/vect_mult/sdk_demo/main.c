55bf4460638b78ebb9e1a2976b66b8bc47ea7680
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2023-09-12 21:48:19 +0000

    Added host code for HLS driver

---
+++ examples/vect_mult/sdk_demo/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Add header file for calling the HLS IP
+#include "xvect_mult.h"
+
+// HLS IP instance
+#define XVECT_MULT_0_DEVICE_ID 0
+XVect_mult vect_mult;
+int vect_mult_done = 0;
+
+static int setup_interrupt() {
+  // JC: Need to replace this with RSIC V
+  // int result;
+  // XScuGic_Config *pCfg = XScuGic_LookupConfig(XPAR_SCUGIC_SINGLE_DEVICE_ID);
+  // if (pCfg == NULL) {
+  //   print("Interrupt Configuration Lookup Failed\n\r");
+  //   return XST_FAILURE;
+  // }
+  // result = XScuGic_CfgInitialize(&ScuGic, pCfg, pCfg->CpuBaseAddress);
+  // if (result != XST_SUCCESS) {
+  //   return result;
+  // }
+  // // self test
+  // result = XScuGic_SelfTest(&ScuGic);
+  // if (result != XST_SUCCESS) {
+  //   return result;
+  // }
+
+  // auto result =
+  //     XScuGic_Connect(&ScuGic, XPAR_FABRIC_SVM_CLASSIFIER_0_INTERRUPT_INTR,
+  //                     (Xil_InterruptHandler)hls_classifier_isr, &classifier);
+  // if (result != XST_SUCCESS) {
+  //   return result;
+  // }
+  // XScuGic_Enable(&ScuGic, XPAR_FABRIC_SVM_CLASSIFIER_0_INTERRUPT_INTR);
+
+  return XST_SUCCESS;
+}
+
+static int hls_vect_mult_init(XVect_mult *vect_mult_ptr) {
+  int status = XVect_mult_Initialize(vect_mult_ptr, "vect_mult");
+  if (status != XST_SUCCESS) {
+    printf("ERROR: Could not initialize accelerator.\n");
+    return XST_DEVICE_NOT_FOUND;
+  }
+  return status;
+}
+
+static void hls_vect_mult_start(void *vect_mult_ptr) {
+  XVect_mult *pAccelerator = (XVect_mult *)vect_mult_ptr;
+  XVect_mult_InterruptEnable(pAccelerator, 1);
+  XVect_mult_InterruptGlobalEnable(pAccelerator);
+  XVect_mult_Start(pAccelerator);
+}
+
+static void hls_vect_mult_isr(void *vect_mult_ptr) {
+  XVect_mult *pAccelerator = (XVect_mult *)vect_mult_ptr;
+  XVect_mult_InterruptClear(pAccelerator, 1);
+
+  // Fetch result C
+  int c = XVect_mult_Get_c(vect_mult_ptr);
+  vect_mult_done = 1;
+}
+
+int main() {
+  int status;
+
+  // Setup the matrix mult
+  status = hls_vect_mult_init(&vect_mult);
+  if (status != XST_SUCCESS) {
+    printf("HLS peripheral setup failed\n\r");
+    exit(-1);
+  }
+  // Setup the interrupt
+  status = setup_interrupt();
+  if (status != XST_SUCCESS) {
+    printf("Interrupt setup failed\n\r");
+    exit(-1);
+  }
+
+  if (XVect_mult_IsReady(&vect_mult))
+    printf("HLS peripheral is ready.  Starting... ");
+  else {
+    printf("!!! HLS peripheral is not ready! Exiting...\n\r");
+    exit(-1);
+  }
+
+  // Initialize data
+  int size = 100;
+  XVect_mult_Set_size(&vect_mult, size);
+
+  int a[1000], b[1000];
+  int *buffer_a = &a[0];
+  int *buffer_b = &b[0];
+  XVect_mult_Set_a(&vect_mult, buffer_a);
+  XVect_mult_Set_b(&vect_mult, buffer_b);
+
+  hls_vect_mult_start(&vect_mult);
+
+  while (!vect_mult_done)
+    ;
+  return 0;
+}
---
+++ examples/vect_mult/sdk_demo/xvect_mult_hw.h
@@ -0,0 +1,60 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2023.1 (64-bit)
+// Tool Version Limit: 2023.05
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+// 
+// ==============================================================
+// control
+// 0x00 : Control signals
+//        bit 0  - ap_start (Read/Write/COH)
+//        bit 1  - ap_done (Read/COR)
+//        bit 2  - ap_idle (Read)
+//        bit 3  - ap_ready (Read/COR)
+//        bit 7  - auto_restart (Read/Write)
+//        bit 9  - interrupt (Read)
+//        others - reserved
+// 0x04 : Global Interrupt Enable Register
+//        bit 0  - Global Interrupt Enable (Read/Write)
+//        others - reserved
+// 0x08 : IP Interrupt Enable Register (Read/Write)
+//        bit 0 - enable ap_done interrupt (Read/Write)
+//        bit 1 - enable ap_ready interrupt (Read/Write)
+//        others - reserved
+// 0x0c : IP Interrupt Status Register (Read/TOW)
+//        bit 0 - ap_done (Read/TOW)
+//        bit 1 - ap_ready (Read/TOW)
+//        others - reserved
+// 0x10 : Data signal of size
+//        bit 31~0 - size[31:0] (Read/Write)
+// 0x14 : reserved
+// 0x18 : Data signal of a
+//        bit 31~0 - a[31:0] (Read/Write)
+// 0x1c : Data signal of a
+//        bit 31~0 - a[63:32] (Read/Write)
+// 0x20 : reserved
+// 0x24 : Data signal of b
+//        bit 31~0 - b[31:0] (Read/Write)
+// 0x28 : Data signal of b
+//        bit 31~0 - b[63:32] (Read/Write)
+// 0x2c : reserved
+// 0x30 : Data signal of c
+//        bit 31~0 - c[31:0] (Read/Write)
+// 0x34 : Data signal of c
+//        bit 31~0 - c[63:32] (Read/Write)
+// 0x38 : reserved
+// (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
+
+#define XVECT_MULT_CONTROL_ADDR_AP_CTRL   0x00
+#define XVECT_MULT_CONTROL_ADDR_GIE       0x04
+#define XVECT_MULT_CONTROL_ADDR_IER       0x08
+#define XVECT_MULT_CONTROL_ADDR_ISR       0x0c
+#define XVECT_MULT_CONTROL_ADDR_SIZE_DATA 0x10
+#define XVECT_MULT_CONTROL_BITS_SIZE_DATA 32
+#define XVECT_MULT_CONTROL_ADDR_A_DATA    0x18
+#define XVECT_MULT_CONTROL_BITS_A_DATA    64
+#define XVECT_MULT_CONTROL_ADDR_B_DATA    0x24
+#define XVECT_MULT_CONTROL_BITS_B_DATA    64
+#define XVECT_MULT_CONTROL_ADDR_C_DATA    0x30
+#define XVECT_MULT_CONTROL_BITS_C_DATA    64
+
