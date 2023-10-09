8f0ea4b6513427292c968584b2fb7ba56ccf21f9
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2023-10-09 14:51:39 +0000

    Refactored C code for non cap one

--- examples/vect_mult/sdk_demo/main.c
+++ examples/vect_mult/sdk_demo/main.c
@@ -1,50 +1,43 @@
-// Add header file for calling the HLS IP
-// #include "cheribgas_de10_bare.h"
-// #include "uart16550.h"
 #include "xvect_mult.h"
 #include <stdint.h>
 
 // HLS IP instance
 #define NUM 8
+#define SIZE 10
 XVect_mult vect_mult_insts[NUM];
 u64 base_phy_addr[NUM] = {0xC0010000, 0xC0011000, 0xC0012000, 0xC0013000,
                           0xC0014000, 0xC0015000, 0xC0016000, 0xC0017000};
-u64 base_buff_phy_addr[NUM] = {0x90010000, 0x90011000, 0x90012000, 0x90013000,
-                               0x90014000, 0x90015000, 0x90016000, 0x90017000};
+u32 a[NUM][SIZE];
+u32 b[NUM][SIZE];
+u32 c[NUM][SIZE];
+u32 c_gold[NUM][SIZE];
 
-int test_case = 0;
-
-volatile int vect_mult_done = 0;
-volatile char *uart_base = 0xC0000000;
-
-void success() {
-
-  *uart_base = 's';
+volatile void success() {
   while (1)
     ;
 }
 
-void fail() {
+volatile void fail() {
   while (1)
     ;
 }
 
-u32 c[NUM][10];
+u32 hls_vect_mult_init(int test_case, u64 phy) {
 
-static u32 hls_vect_mult_start(XVect_mult *top, u64 base_address,
-                               u64 base_buffer_address) {
-
-  top->Control_BaseAddress = base_address;
+  XVect_mult *top = vect_mult_insts + test_case;
+  top->Control_BaseAddress = phy;
 
   if (!XVect_mult_IsReady(top))
     return 4;
 
-  int size = 10;
-  XVect_mult_Set_size(top, size);
+  XVect_mult_Set_size(top, SIZE);
 
-  u64 buffer_a = base_buffer_address;
-  u64 buffer_b = base_buffer_address + 100;
-  u64 buffer_c = base_buffer_address + 200;
+  u64 buffer_a = a[test_case];
+  // base_buffer_address;
+  u64 buffer_b = b[test_case];
+  // base_buffer_address + 100;
+  u64 buffer_c = c[test_case];
+  // base_buffer_address + 200;
 
   XVect_mult_WriteReg(top->Control_BaseAddress,
                       XVECT_MULT_CONTROL_ADDR_A_DATA + 4, (u32)(0));
@@ -59,70 +52,23 @@ static u32 hls_vect_mult_start(XVect_mult *top, u64 base_address,
   XVect_mult_WriteReg(top->Control_BaseAddress, XVECT_MULT_CONTROL_ADDR_C_DATA,
                       (u32)(buffer_c));
 
-  u64 low_a = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                 XVECT_MULT_CONTROL_ADDR_A_DATA);
-  u64 high_a = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                  XVECT_MULT_CONTROL_ADDR_A_DATA + 4);
-  u64 new_a = low_a | (high_a << 32);
-  if (high_a != 0)
-    return 1;
-  if (buffer_a != low_a)
-    return 1;
-  if (buffer_a != new_a)
-    return 1;
-
-  u64 low_b = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                 XVECT_MULT_CONTROL_ADDR_B_DATA);
-  u64 high_b = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                  XVECT_MULT_CONTROL_ADDR_B_DATA + 4);
-  u64 new_b = low_b | (high_b << 32);
-  if (high_b != 0)
-    return 2;
-  if (buffer_b != low_b)
-    return 2;
-  if (buffer_b != new_b)
-    return 2;
-
-  u64 low_c = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                 XVECT_MULT_CONTROL_ADDR_C_DATA);
-  u64 high_c = XVect_mult_ReadReg(top->Control_BaseAddress,
-                                  XVECT_MULT_CONTROL_ADDR_C_DATA + 4);
-  u64 new_c = low_c | (high_c << 32);
-  if (high_c != 0)
-    return 3;
-  if (buffer_c != low_c)
-    return 3;
-  if (buffer_c != new_c)
-    return 3;
-
-  u32 *xa = (volatile u32 *)(buffer_a);
-  u32 *xb = (volatile u32 *)(buffer_b);
-  u32 *xc = (volatile u32 *)(buffer_c);
-
-  u32 a[10], b[10];
-
   for (int i = 0; i < 10; i++) {
-    a[i] = i + test_case;
-    b[i] = i + test_case;
-    c[test_case][i] = (i + test_case) * (i + test_case);
-
-    *(xa + i) = i + test_case;
-    *(xb + i) = i + test_case;
-    *(xc + i) = 0;
+    a[test_case][i] = i + test_case;
+    b[test_case][i] = i + test_case;
+    c_gold[test_case][i] = (i + test_case) * (i + test_case);
+    c[test_case][i] = 0;
   }
 
-  test_case++;
-  return buffer_c;
+  return 0;
 }
 
 int main() {
   int status;
 
   // Initialize
-  u32 c_addr[8];
   for (int i = 0; i < NUM; i++)
-    c_addr[i] = hls_vect_mult_start(vect_mult_insts + i, base_phy_addr[i],
-                                    base_buff_phy_addr[i]);
+    if (hls_vect_mult_init(i, base_phy_addr[i]))
+      return 4;
 
   // Compute
   asm("fence");
@@ -135,15 +81,13 @@ int main() {
 
   u32 res = 0;
   for (int n = 0; n < NUM; n++) {
-    u32 *xc = (volatile u32 *)(c_addr[n]);
-    for (int i = 0; i < 10; i++) {
-      u32 c_value = *(xc + i);
-      res += ((u32)c_value == c[n][i]);
+    for (int i = 0; i < SIZE; i++) {
+      res += (c_gold[n][i] == c[n][i]);
     }
   }
 
   // Check
-  if (res == NUM * 10) {
+  if (res == NUM * SIZE) {
     success();
     return 8;
   } else {
