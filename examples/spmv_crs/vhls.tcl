a9040777b8a8d038868f5e56b78a39ebf68aeacd
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-01-18 14:35:25 +0000

    Added spmv crs

---
+++ examples/spmv_crs/vhls.tcl
@@ -0,0 +1,15 @@
+open_project spmv_prj
+
+add_files spmv.c
+add_files -tb spmv.c
+
+set_top hls_top 
+
+open_solution -reset solution
+set_part "xqzu29dr-ffrf1760-1-i"
+create_clock -period 10
+csim_design
+csynth_design
+cosim_design 
+
+exit
