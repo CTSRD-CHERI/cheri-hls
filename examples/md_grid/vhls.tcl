fc65a8937c4076ac75edea70c62f041ad93aff2e
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-01-19 08:46:11 +0000

    Added md grid

---
+++ examples/md_grid/vhls.tcl
@@ -0,0 +1,15 @@
+open_project md_prj
+
+add_files md.c 
+add_files -tb md.c 
+
+set_top hls_top 
+
+open_solution -reset solution
+set_part "xqzu29dr-ffrf1760-1-i"
+create_clock -period 10
+csim_design
+csynth_design
+cosim_design
+
+exit
