8788d6f741c507c0a6722e942afc1433eeb9a03c
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-02-25 10:58:00 +0000

    Added host and cpu code

--- examples/bfs_queue/bare_metal_cpu/main.c
+++ examples/bfs_queue/bare_metal_cpu/main.c
@@ -37,41 +37,53 @@ typedef struct node_t_struct {
 } node_t;
 typedef int level_t;
 
+#define Q_PUSH(node)                                                           \
+  {                                                                            \
+    queue[q_in == 0 ? N_NODES - 1 : q_in - 1] = node;                          \
+    q_in = (q_in + 1) % N_NODES;                                               \
+  }
+#define Q_PEEK() (queue[q_out])
+#define Q_POP()                                                                \
+  { q_out = (q_out + 1) % N_NODES; }
+#define Q_EMPTY()                                                              \
+  (q_in > q_out ? q_in == q_out + 1 : (q_in == 0) && (q_out == N_NODES - 1))
+
 void hls_top(node_index_t starting_node, int levels, int node,
              node_t nodes[N_NODES], edge_t edges[N_EDGES],
-             level_t level[N_NODES], edge_index_t level_counts[N_LEVELS]) {
+             level_t level[N_NODES], edge_index_t level_counts[N_LEVELS], ) {
 
+  node_index_t queue[N_NODES];
+  node_index_t q_in, q_out;
+  node_index_t dummy;
   node_index_t n;
   edge_index_t e;
-  level_t horizon;
-  edge_index_t cnt;
 
+  q_in = 1;
+  q_out = 0;
   level[starting_node] = 0;
   level_counts[0] = 1;
+  Q_PUSH(starting_node);
 
-loop_horizons:
-  for (horizon = 0; horizon < levels; horizon++) {
-    cnt = 0;
-  // Add unmarked neighbors of the current horizon to the next horizon
-  loop_nodes:
-    for (n = 0; n < node; n++) {
-      if (level[n] == horizon) {
-        edge_index_t tmp_begin = nodes[n].edge_begin;
-        edge_index_t tmp_end = nodes[n].edge_end;
-      loop_neighbors:
-        for (e = tmp_begin; e < tmp_end; e++) {
-          node_index_t tmp_dst = edges[e].dst;
-          level_t tmp_level = level[tmp_dst];
+loop_queue:
+  for (dummy = 0; dummy < node; dummy++) { // Typically while(not_empty(queue)){
+    if (Q_EMPTY())
+      break;
+    n = Q_PEEK();
+    Q_POP();
+    edge_index_t tmp_begin = nodes[n].edge_begin;
+    edge_index_t tmp_end = nodes[n].edge_end;
+  loop_neighbors:
+    for (e = tmp_begin; e < tmp_end; e++) {
+      node_index_t tmp_dst = edges[e].dst;
+      level_t tmp_level = level[tmp_dst];
 
-          if (tmp_level == 255) { // Unmarked
-            level[tmp_dst] = horizon + 1;
-            ++cnt;
-          }
-        }
+      if (tmp_level == MAX_LEVEL) { // Unmarked
+        level_t tmp_level = level[n] + 1;
+        level[tmp_dst] = tmp_level;
+        ++level_counts[tmp_level];
+        Q_PUSH(tmp_dst);
       }
     }
-    if ((level_counts[horizon + 1] = cnt) == 0)
-      break;
   }
 }
 
