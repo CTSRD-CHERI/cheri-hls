b0c7f42aa1f546f6f027e81dce4b45338465e2f3
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-05-23 23:11:31 +0000

    work

--- examples/backprop/backprop.c
+++ examples/backprop/backprop.c
@@ -276,7 +276,7 @@ void hls_top(int sets, int xweights1[input_dimension * nodes_per_layer],
              int xbiases1[nodes_per_layer], int xbiases2[nodes_per_layer],
              int xbiases3[possible_outputs],
              int xtraining_data[training_sets * input_dimension],
-             int xtraining_targets[training_sets * possible_outputs]) {
+             int xtraining_targets[training_sets][possible_outputs]) {
 #pragma HLS INTERFACE m_axi port = xweights1
 #pragma HLS INTERFACE m_axi port = xweights2
 #pragma HLS INTERFACE m_axi port = xweights3
@@ -295,14 +295,15 @@ void hls_top(int sets, int xweights1[input_dimension * nodes_per_layer],
   int biases2[nodes_per_layer] = {1};
   int biases3[possible_outputs] = {1};
   int training_data[training_sets][input_dimension] = {1};
-  int training_targets[training_sets * possible_outputs] = {1};
+  int training_targets[training_sets][possible_outputs] = {1};
 
   for (int i = 0; i < training_sets; i++)
     for (int j = 0; j < input_dimension; j++)
       training_data[i][j] = xtraining_data[i * input_dimension + j];
 
-  for (int i = 0; i < training_sets * possible_outputs; i++)
-    training_targets[i] = xtraining_targets[i];
+  for (int i = 0; i < training_sets; i++)
+    for (int j = 0; j < possible_outputs; j++)
+      training_targets[i][j] = xtraining_targets[i * possible_outputs + j];
 
   int i, j;
   // Forward and training structures
@@ -339,25 +340,21 @@ void hls_top(int sets, int xweights1[input_dimension * nodes_per_layer],
                                                  activations3, activations2);
     RELU(activations3, dactivations3, possible_outputs);
 
-    // soft_max(net_outputs, activations3);
-    // take_difference(net_outputs, training_targets[i],
-    //                 output_difference, dactivations3);
-    // get_delta_matrix_weights3(delta_weights3, output_difference,
-    // activations2); get_oracle_activations2(weights3, output_difference,
-    // oracle_activations2,
-    //                         dactivations2);
-    // get_delta_matrix_weights2(delta_weights2, oracle_activations2,
-    //                           activations1);
-    // get_oracle_activations1(weights2, oracle_activations2,
-    // oracle_activations1,
-    //                         dactivations1);
-    // get_delta_matrix_weights1(delta_weights1, oracle_activations1,
-    //                           training_data[i]);
-    // update_weights(weights1, weights2, weights3, delta_weights1,
-    // delta_weights2,
-    //                delta_weights3, biases1, biases2, biases3,
-    //                oracle_activations1, oracle_activations2,
-    //                output_difference);
+    soft_max(net_outputs, activations3);
+    take_difference(net_outputs, training_targets[i], output_difference,
+                    dactivations3);
+    get_delta_matrix_weights3(delta_weights3, output_difference, activations2);
+    get_oracle_activations2(weights3, output_difference, oracle_activations2,
+                            dactivations2);
+    get_delta_matrix_weights2(delta_weights2, oracle_activations2,
+                              activations1);
+    get_oracle_activations1(weights2, oracle_activations2, oracle_activations1,
+                            dactivations1);
+    get_delta_matrix_weights1(delta_weights1, oracle_activations1,
+                              training_data[i]);
+    update_weights(weights1, weights2, weights3, delta_weights1, delta_weights2,
+                   delta_weights3, biases1, biases2, biases3,
+                   oracle_activations1, oracle_activations2, output_difference);
   }
 
   for (int i = 0; i < input_dimension * nodes_per_layer; i++)
