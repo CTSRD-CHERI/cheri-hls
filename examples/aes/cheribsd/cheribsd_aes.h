d8afc381d9c670d7f43c082f80e0f2c4318035bf
Jianyi Cheng <jianyi.cheng@cl.cam.ac.uk>
2024-06-07 02:00:24 +0000

    Added init files

---
+++ examples/aes/cheribsd/cheribsd_aes.h
@@ -0,0 +1,22 @@
+#include <sys/accel.h>
+
+#define row_size 64
+#define col_size 64
+#define N row_size *col_size
+
+static void init_buffer_config(struct buffer_config *bc, int o, int w, int b) {
+  bc->offset = o;
+  bc->addr_width = w;
+  bc->size = b;
+  bc->data = malloc(b * sizeof(int));
+}
+
+void init_xaes(struct accel_config *x) {
+  x->buffer_count = 4;
+  x->buffers = malloc((buffer_config)*x->buffer_count);
+
+  init_buffer_config(x->buffers[0], 0x10, 32, 1);
+  init_buffer_config(x->buffers[1], 0x18, 64, N);
+  init_buffer_config(x->buffers[2], 0x24, 64, N);
+  init_buffer_config(x->buffers[3], 0x30, 64, N);
+}
---
+++ examples/stencil2d/cheribsd/main.c
@@ -0,0 +1,47 @@
+#include <sys/accel.h>
+#include <sys/module.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "cheribsd_stencil2d.h"
+
+#define NUM 8
+
+int main(int argc __unused, char **argv __unused) {
+
+  struct accel_ctrl_args aca;
+
+  // Initialize accelerator processes
+  aca.accel_count = NUM;
+  aca.accels = malloc((accel_config)*aca.accel_count);
+  int bytes = 0;
+  for (int i = 0; i < NUM; i++) {
+    init_xstencil2d(aca.accels[i]);
+    bytes += aca.accel[i].bytes;
+  }
+  aca.bytes = bytes;
+
+  // Initialize buffer data
+  for (int i = 0; i < NUM; i++)
+    for (int j = 0; j < aca.accels[i]->buffer_count; j++) {
+      aca.accels[i]->buffers[j]->data = malloc((int)*aca.accel_count);
+      for (int k = 0; k < aca.accels[i]->buffers[j]->size; k++)
+        aca.accels[i]->buffers[j]->data[k] = 1;
+    }
+
+  rc = __syscall(584, &aca);
+  rc = __syscall(585, &aca);
+
+  for (int i = 0; i < NUM; i++)
+    for (int j = 0; j < aca.accels[i]->buffer_count; j++)
+      for (int k = 0; k < aca.accels[i]->buffers[j]->size; k++)
+        print("buffer value: accelerator %d buffer %d element %d = %d\n", i, j,
+              k, aca.accels[i]->buffers[j]->data[k]);
+
+  return rc;
+}
