3376668c3fe667f839347891abb6aa304cb505ba
Jianyi Cheng <jianyi.cheng@ed.ac.uk>
2025-10-26 16:45:36 +0000

    fixed sort_radix

--- hls_examples/sort_radix/full/sort.cpp
+++ hls_examples/sort_radix/full/sort.cpp
@@ -6,7 +6,7 @@ benchmark suite. In Proceedings of the 3rd Workshop on General-Purpose
 Computation on Graphics Processing Units, 2010
 */
 
-#include "../../chls-fullcap.h"
+#include "../../chls-fullcap-inline.h"
 
 #define TYPE int
 
